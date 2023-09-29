6f7e52c3696d29e8818387c69bfd763c9e83d36c
darshan15062002 <darshanjain15062002@gmail.com>
2023-09-29 21:44:30 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = () => {
-    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifideTopology: true, useCreateIndex: true }).then((data) => {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
         console.log(`mongoDB connected with server ${data.connection.host}`);
     }).catch((er) => { console.log(er); })
 }
