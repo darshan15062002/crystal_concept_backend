ee41f13adeb66e29b24a9c8615cd20ea5ec1df6a
darshan15062002 <darshanjain15062002@gmail.com>
2023-09-30 12:38:49 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,16 +1,9 @@
 const mongoose = require('mongoose')
 
+const connectDB = () => {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
+        console.log(`mongoDB connected with server ${data.connection.host}`);
+    }).catch((er) => { console.log(er); })
+}
 
-
-
-const connectDB = async () => {
-    try {
-        const { connection } = await mongoose.connect(process.env.DB_URI);
-
-        console.log(`Server connected to database ${connection.host}`);
-    } catch (error) {
-        console.log("Some Error Occurred", error);
-        process.exit(1);
-    }
-};
 module.exports = connectDB
