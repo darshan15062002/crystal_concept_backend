c7db26a90e84809f82e7097c75189cdca1167d54
darshan15062002 <darshanjain15062002@gmail.com>
2023-09-30 13:27:13 +0530

    update

--- config/database.js
+++ config/database.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose')
 
 const connectDB = () => {
-    mongoose.connect("mongodb+srv://darshan:$$dar$$123@cluster0.ak2icim.mongodb.net/Student", { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
+    mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
         console.log(`mongoDB connected with server ${data.connection.host}`);
     }).catch((er) => { console.log(er); })
 }
