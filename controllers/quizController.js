10d4e19a0f344bc65081e823640370b8c486fff8
darshan15062002 <darshanjain15062002@gmail.com>
2024-02-21 19:44:38 +0530

    SDf

--- controllers/quizController.js
+++ controllers/quizController.js
@@ -120,6 +120,7 @@ exports.getAllGeneratedQuizzes = catchAsyncError(async (req, res, next) => {
     }
 
     const client = new TextServiceClient({
+
         authClient: new GoogleAuth().fromAPIKey(API_KEY),
     });
 
