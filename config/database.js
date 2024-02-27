d2a4dee6f86624cd71dbe12eae8de3fe41209a39
darshan15062002 <darshanjain15062002@gmail.com>
2024-02-28 00:27:38 +0530

    Sdf

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose')
-
+console.log(process.env.DB_URL);
 const connectDB = () => {
     mongoose.connect(process.env.DB_URL, { useNewUrlParser: true, useUnifiedTopology: true }).then((data) => {
         console.log(`mongoDB connected with server ${data.connection.host}`);
